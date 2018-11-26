6a8d09eca3fd47351d72430b3bc287f05372dd45
Valentino Pesce <valentino@iltuobrand.it>
2018-11-26 11:07:02 +0100

    added PDO

--- Schema/Mysql.php
+++ Schema/Mysql.php
@@ -2,9 +2,11 @@
 
 namespace Kanboard\Plugin\EncryptedContent\Schema;
 
+use PDO;
+
 const VERSION = 1;
 
-function version_1($pdo)
+function version_1(PDO $pdo)
 {
     $pdo->exec('ALTER TABLE `task_has_metadata` CHANGE `value` `value` MEDIUMTEXT');
 }
