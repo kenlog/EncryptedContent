16c57d9801b1be3758394695a93adb40e8d6f958
Craig Crosby <creecros@gmail.com>
2018-11-27 12:52:09 -0500

    Update Sqlite.php

--- Schema/Sqlite.php
+++ Schema/Sqlite.php
@@ -10,7 +10,7 @@ function version_1(PDO $pdo)
 {
     $pdo->exec("CREATE TABLE IF NOT EXISTS task_has_encrypted_content (
         task_id INTEGER NOT NULL,
-        name INTEGER PRIMARY KEY,
+        name INTEGER PRIMARY KEY AUTOINCREMENT,
         value TEXT NOT NULL,
         changed_by INTEGER NOT NULL DEFAULT 0,
         changed_on INTEGER NOT NULL DEFAULT 0
