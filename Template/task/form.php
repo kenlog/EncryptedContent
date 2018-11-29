1c0d9467ddf5bffa789dfcfab79c05abb4af8395
Valentino Pesce <valentino@iltuobrand.it>
2018-11-29 09:06:20 +0100

    Update form.php

--- Template/task/form.php
+++ Template/task/form.php
@@ -1,7 +1,7 @@
 <h2><?= $form_headline ?></h2>
 <form method="post" action="<?= $this->url->href('EncryptedContentController', 'updateTask', ['plugin' => 'encryptedContent', 'task_id' => $task['id'], 'project_id' => $project['id']]) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
-        <?= $this->form->input('hidden', 'name', $values, [], ['placeholder="'.t('Name').'"']) ?>
+        <?= $this->form->hidden('name', $values) ?>
     <div>
         <?= $this->form->label(t('Content'), 'Content') ?>
         <?= $this->EncryptedContentHelper->renderEncryptedtextEditor('value', $values, ['required', 'placeholder="'.t('Content').'"']) ?>
