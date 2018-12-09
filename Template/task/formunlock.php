3bcece12f82239cebb4dfad99526dbc7416e6aeb
kenlog <valentino@iltuobrand.it>
2018-12-09 09:13:53 +0100

    added autofocus to the key field

--- Template/task/formunlock.php
+++ Template/task/formunlock.php
@@ -4,7 +4,7 @@
         <?= $this->form->hidden('name', $values) ?>
     <div style="margin-bottom:20px">
         <?= $this->form->label(t('Key'), 'key') ?>
-        <?= $this->EncryptedContentHelper->input('password','key', $values, ['required', 'placeholder="'.t('Key').'"']) ?>
+        <?= $this->EncryptedContentHelper->input('password','key', $values, ['autofocus', 'required', 'placeholder="'.t('Key').'"']) ?>
     </div>
     <input type="submit" value="<?= t('Unlock') ?>" class="btn btn-blue"/>
 </form>
