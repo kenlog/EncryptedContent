70c0298982834529a666aa3db815df1643c8c524
kenlog <valentino@iltuobrand.it>
2018-12-09 19:10:48 +0100

    update translation

--- Locale/it_IT/translations.php
+++ Locale/it_IT/translations.php
@@ -19,5 +19,6 @@ return [
     'Unlock Encrypted Content' => 'Sblocca il contenuto crittografato',
     'Content decrypt' => 'Contenuto decifrato',
     'Random key copy' => 'Copia chiave casuale',
-    'Important: saving this key in a password manager and keeping it confidential will be necessary to decrypt this content in the future.' => 'Importante: salvare questa chiave in un gestore di password e mantenerla riservata sarà necessaria per decifrare questo contenuto in futuro.'
+    'Important: saving this key in a password manager and keeping it confidential will be necessary to decrypt this content in the future.' => 'Importante: salvare questa chiave in un gestore di password e mantenerla riservata sarà necessaria per decifrare questo contenuto in futuro.',
+    'This feature requires the use of an SSL certificate on the server' => 'Questa funzione richiede l\'uso di un certificato SSL sul server'
 ];
