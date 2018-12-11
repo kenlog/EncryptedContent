e5467a990ae002f7fc23ac414659d6bbf7ca0e8e
Craig Crosby <creecros@gmail.com>
2018-12-11 09:29:57 -0500

    Update formnew.php

--- Template/task/formnew.php
+++ Template/task/formnew.php
@@ -1,7 +1,7 @@
 <h2><?= $form_headline ?></h2>
 <small>
-    <button id="randomKey" class="btn btn-blue" data-clipboard-text="<?= $this->EncryptedContentHelper->generateNewRandomKey() ?>" style="margin-bottom:10px"><?= t('Random key copy') ?></button>
-    <button id="randomKey" class="btn btn-red" data-clipboard-text="<?= t('The key has been deleted from the clipboard') ?>" style="margin-bottom:10px"><?= t('Delete the key from the clipboard') ?></button>
+    <button id="randomKey" class="btn btn-blue" data-clipboard-text="<?= $this->EncryptedContentHelper->generateNewRandomKey() ?>" style="margin-bottom:10px"><?= t('Copy random key') ?></button>
+    <button id="randomKey" class="btn btn-red" data-clipboard-text="<?= t('The key has been removed from the clipboard') ?>" style="margin-bottom:10px"><?= t('Remove key from clipboard') ?></button>
     <p class="alert"><b><?= t('Save this key in a password manager to keep it confidential and use it to read the encrypted content in the future.') ?></b></p>
 </small>
 <form method="post" action="<?= $this->url->href('EncryptedContentController', 'saveTask', ['plugin' => 'encryptedContent', 'task_id' => $task['id'], 'project_id' => $project['id']]) ?>" autocomplete="off">
