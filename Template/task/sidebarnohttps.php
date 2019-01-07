e74651fc824efe68344fc497d68475215ade8466
Valentino Pesce <valentino@iltuobrand.it>
2019-01-07 12:56:07 +0100

    Fix: icon

--- Template/task/sidebarnohttps.php
+++ Template/task/sidebarnohttps.php
@@ -1,6 +1,5 @@
 <?php if ($this->user->hasProjectAccess('metadata', 'index', $task['project_id'])): ?>
         <li <?= $this->app->checkMenuSelection('EncryptedContentController', 'noHTTPS') ?>>
-        <i class="fa fa-lock fa-fw"></i>
-        <?= $this->url->link(t('Encrypted Content'), 'EncryptedContentController', 'noHTTPS', ['plugin' => 'encryptedContent', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>
+        <?= $this->url->icon('lock',t('Encrypted Content'), 'EncryptedContentController', 'noHTTPS', ['plugin' => 'encryptedContent', 'task_id' => $task['id'], 'project_id' => $task['project_id']]) ?>
         </li>
 <?php endif ?>
